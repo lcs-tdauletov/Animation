1427928b944029a5a107c09915ed3b05d3808dd5
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2017-11-18 23:55:39 -0500

    The two ellipses and a line that appears in-between their origins.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,9 +9,11 @@ class Sketch : NSObject {
     // Position of circle
     var xbig : Int
 
-    
+    var dxbig : Int
     var xsmall : Int
     var ysmall : Int
+    var dxsmall : Int
+    var dysmall : Int
     
     // This function runs once
     override init() {
@@ -20,11 +22,15 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        xbig = 0
+        xbig = 250
        
         
         xsmall = 0
         ysmall = 100
+        dxbig = 3
+        dxsmall = 4
+        dysmall = 4
+        
     }
     
     // Runs in a loop, forever, to create the animated effect
@@ -32,23 +38,60 @@ class Sketch : NSObject {
         
         // Change position
         canvas.fillColor = Color.white
-        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+        
+       // canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+        
         canvas.fillColor = Color.black
-        xbig += 1
+        
+        xbig += dxbig
+        
+        xsmall += dxsmall
+        
+        ysmall += dysmall
       
-        xsmall += 2
-        ysmall += 2
+        
+        
+        
+        
+        if xbig > 400 {
+            dxbig = -3
+        }
+        if xbig < 100 {
+            dxbig = 3
+        }
+        
+        
+        
+        if xsmall > 475 {
+            dxsmall = -4
+        }
+        if xsmall < 25 {
+            dxsmall = 4
+        }
+        if ysmall > 475 {
+            dysmall = -4
+        }
+        if ysmall < 25 {
+            dysmall = 4
+        }
+        
         canvas.drawShapesWithFill = false
         canvas.drawEllipse(centreX: xbig, centreY: 250, width: 200, height: 200)
         canvas.drawEllipse(centreX: xbig, centreY: 250, width: 1, height: 1)
         
         
         
-        canvas.drawShapesWithFill = true
-    
-    
-    
+        canvas.drawEllipse(centreX: xsmall, centreY: ysmall, width: 50, height: 50)
+        canvas.drawEllipse(centreX: xsmall, centreY: ysmall, width: 1, height: 1)
         
-    }
+        if sqrt(pow(Double(xbig - xsmall), 2) + pow(Double(250 - ysmall), 2)) < 125 {
+            canvas.lineColor = Color.red
+            canvas.drawLine(fromX: xbig, fromY: 250, toX: xsmall, toY: ysmall)
+        
+        }
+        
+        canvas.drawShapesWithFill = true
+        
+        }
     
 }
