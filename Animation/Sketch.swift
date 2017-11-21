4181b660ef2cf81b87dad28fc61953245eaff760
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2017-11-21 18:54:00 -0500

    Finally did a sine wave with randomization in it.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,8 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var y : Int
+    var direction : Bool = true
     
     // This function runs once
     override init() {
@@ -16,19 +18,29 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
-        
+        x = 0
+        y = canvas.height / 2
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
         // Change position
-        x += 1
+        if direction == true {
+            x += 1
+        } else {
+            x -= 1
+        }
+        y = Int(sin(Double(x) / 20) * 40) + canvas.height / 2
         
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: y, width: 10, height: 10)
         
+        if x > canvas.width || x < 0 {
+            direction = !direction
+            
+            canvas.fillColor = Color(hue: random(from: 0, toButNotIncluding: 361), saturation: 100, brightness: 100, alpha: 100)
+        }
     }
     
 }
